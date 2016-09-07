96c5507ced953d910b56f0b6222ad0bce20387f7
ritomar <ritomar@hotmail.com>
2016-09-07 20:04:07 -0300

    Happy numbers recursivo 2

--- mod02/happynumbers/happynumbers.py
+++ mod02/happynumbers/happynumbers.py
@@ -1,14 +1,6 @@
-def sum_of_squares(number):
-    string = str(number)
-    digits = [int(char) ** 2 for char in string]
-    return sum(digits)
-
 def happy(number):
+    next_ = sum(int(char) ** 2 for char in str(number))
+    return number in (1, 7) if number < 10 else happy(next_)
 
-    if number < 10:
-        return number in (1, 7)
-    return happy(sum_of_squares(number))
-
-assert sum_of_squares(130) == 10
 assert all(happy(n) for n in (1, 10, 100, 130, 97))
 assert not all(happy(n) for n in (2, 3, 4, 5, 6, 8, 9))
